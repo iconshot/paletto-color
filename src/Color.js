0f21b4b0ffc9d55c7e1468f21d89a6d502c6a9c1
josecarlosrx <jc@iconshot.com>
2024-06-26 18:23:46 -0500

    update Color

--- src/Color.js
+++ src/Color.js
@@ -1,7 +1,7 @@
 const ColorConvert = require("color-convert");
 
 class Color {
-  constructor(hue, saturation) {
+  constructor(hue, saturation = 100) {
     this.hue = hue;
     this.saturation = saturation;
   }
@@ -16,55 +16,55 @@ class Color {
 
   // source: http://www.procato.com/rgb+index/
 
-  static hues = {
-    red: 0,
-    scarlet: 7.5,
-    vermilion: 15,
-    tangelo: 22.5,
-    orange: 30,
-    gamboge: 37.5,
-    amber: 45,
-    gold: 52.5,
-    yellow: 60,
-    appleGreen: 67.5,
-    limeGreen: 75,
-    springBud: 82.5,
-    chartreuseGreen: 90,
-    pistachio: 97.5,
-    harlequin: 105,
-    sapGreen: 112.5,
-    green: 120,
-    emeraldGreen: 127.5,
-    malachiteGreen: 135,
-    seaGreen: 142.5,
-    springGreen: 150,
-    aquamarine: 157.5,
-    turquoise: 165,
-    opal: 172.5,
-    cyan: 180,
-    arcticBlue: 187.5,
-    cerulean: 195,
-    cornflowerBlue: 202.5,
-    azure: 210,
-    cobaltBlue: 217.5,
-    sapphireBlue: 225,
-    phthaloBlue: 232.5,
-    blue: 240,
-    persianBlue: 247.5,
-    indigo: 255,
-    blueViolet: 262.5,
-    violet: 270,
-    purple: 277.5,
-    mulberry: 285,
-    heliotrope: 292.5,
-    magenta: 300,
-    orchid: 307.5,
-    fuchsia: 315,
-    cerise: 322.5,
-    rose: 330,
-    raspberry: 337.5,
-    crimson: 345,
-    amaranth: 352.5,
+  static Hues = class Hues {
+    static RED = 0;
+    static SCARLET = 7.5;
+    static VERMILION = 15;
+    static TANGELO = 22.5;
+    static ORANGE = 30;
+    static GAMBOGE = 37.5;
+    static AMBER = 45;
+    static GOLD = 52.5;
+    static YELLOW = 60;
+    static APPLE_GREEN = 67.5;
+    static LIME_GREEN = 75;
+    static SPRING_BUD = 82.5;
+    static CHARTREUSE_GREEN = 90;
+    static PISTACHIO = 97.5;
+    static HARLEQUIN = 105;
+    static SAP_GREEN = 112.5;
+    static GREEN = 120;
+    static EMERALD_GREEN = 127.5;
+    static MALACHITE_GREEN = 135;
+    static SEA_GREEN = 142.5;
+    static SPRING_GREEN = 150;
+    static AQUAMARINE = 157.5;
+    static TURQUOISE = 165;
+    static OPAL = 172.5;
+    static CYAN = 180;
+    static ARCTIC_BLUE = 187.5;
+    static CERULEAN = 195;
+    static CORNFLOWER_BLUE = 202.5;
+    static AZURE = 210;
+    static COBALT_BLUE = 217.5;
+    static SAPPHIRE_BLUE = 225;
+    static PHTHALO_BLUE = 232.5;
+    static BLUE = 240;
+    static PERSIAN_BLUE = 247.5;
+    static INDIGO = 255;
+    static BLUE_VIOLET = 262.5;
+    static VIOLET = 270;
+    static PURPLE = 277.5;
+    static MULBERRY = 285;
+    static HELIOTROPE = 292.5;
+    static MAGENTA = 300;
+    static ORCHID = 307.5;
+    static FUCHSIA = 315;
+    static CERISE = 322.5;
+    static ROSE = 330;
+    static RASPBERRY = 337.5;
+    static CRIMSON = 345;
+    static AMARANTH = 352.5;
   };
 }
 
