24f5b616c6ff759550d811ce89d3a11e1e931e85
josecarlosrx <jc@iconshot.com>
2024-10-11 02:55:16 -0500

    add blank

--- src/Color.ts
+++ src/Color.ts
@@ -36,6 +36,10 @@ export class Color {
     return `#${hex}`;
   }
 
+  public static get blank(): Color {
+    return new Color(0, 0);
+  }
+
   // source: http://www.procato.com/rgb+index/
 
   public static Hues = class Hues {
